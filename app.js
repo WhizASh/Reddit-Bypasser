7e4ff432c83ca6636d3a50a40c1777fb764c41a4
WhizASh <whizash91@gmail.com>
2023-12-29 23:29:45 +0530

    Added the required files

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('!!!!!!!!!!!!REDDIT NSFW BYPASSER!!!!!!!!!!!  \O\
+  \/|\\ \
+  \/ \\')
+
+console.log('O')
+console.log("/|\\")
+console.log('/ \\')
+
+b = location.hostname;
+let block = document.querySelector("body shreddit-app shreddit-async-loader xpromo-nsfw-blocking-modal-desktop ");
+
+if(block!=null)
+{
+    b=b.replace("www.reddit","old.reddit");
+    console.log(b)
+    location.hostname=b;
+    console.log("Enjoy !!")
+
+}
+else{
+    console.log("All safe");
+}
+
+
